e715a74a83b25b7f3609400e10d5243bf1aeb09e
Nkateko Shikuhele <shikuhelenkateko@gmail.com>
2025-08-29 22:49:05 +0200

    implement basic math functions for calculator: add, subtract, multiply, and divide

---
+++ main.js
@@ -0,0 +1,25 @@
+// Your calculator is going to contain functions for all of the basic math operators you typically find on calculators, so start by creating functions for the following items and testing them in your browser’s console:
+
+ // add
+const add = (a,b) => {
+    return a + b
+}
+console.log(add(1,3))
+
+// subtract
+const subtract = (a, b) => {
+  return a - b;
+};
+console.log(subtract(1, 3));
+
+// multiply
+const multiply = (a, b) => {
+  return a * b;
+};
+console.log(multiply(1, 3));
+
+// divide
+const divide = (a, b) => {
+  return a / b;
+};
+console.log(divide(9, 3));
